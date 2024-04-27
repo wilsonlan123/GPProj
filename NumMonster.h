a5da12e3a031dbd9896b8197e6cf94a3b7903ae0
DickySo <159104065+DickySo@users.noreply.github.com>
2024-04-28 00:29:30 +0800

    Update and rename DiceRolling.h to NumMonster.h

---
+++ NumMonster.h
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+void genmonster(int , vector<int>);
+int killmonster(int, vector<int> , int);
+void playersturn(int, string, int, vector<int>);
+bool eliminate(string, string);
