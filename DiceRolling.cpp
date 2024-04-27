9de7218718752ac5023d327daa20910820eb68f3
DickySo <159104065+DickySo@users.noreply.github.com>
2024-04-28 00:28:52 +0800

    Update NumMonster.cpp

--- DiceRolling.cpp
+++ DiceRolling.cpp
@@ -1,131 +1,101 @@
-
 #include <iostream>
 #include <cstdlib>
-#include <ctime>
-#include <iomanip>
-#include<iostream> 
-#include<list>
-#include<vector>
-#include<random>
-#include<map>
-#include<string>
+#include <vector>
+#include <random>
+#include <string>
 using namespace std;
 
-
-void rolling_dice(int pdice, vector<int>& plst) { //function to generate a vector of random dice
-    plst.clear();
-    for (int i = 0; i < pdice; i++) {
-        int roll = rand() % 6 + 1;
-        plst.push_back(roll);
+void genmonster(int m, vector<int>& mlist) { //function for generating the random monster
+    mlist.clear();
+    random_device rd; //to make sure the random number generated is different every time.
+    mt19937 gen(rd()); 
+    uniform_int_distribution<int> dis(1, 9);
+    for (int i = 0; i < m; i++) {
+        int mon = dis(gen);
+        mlist.push_back(mon);
     }
-    return;}
+    return;
+}
 
-int check_number(int choice, vector<int> plst, int pdice) { //function to check whether or not players' guess is in list of dice
-    int number_of_dulplicate = 0;
-    for (int j : plst) {
-        if (j == choice) {
-            number_of_dulplicate++;
+int killmonster(int skillnum, vector<int> monlst, int m) { //function to check whether player can kill the monster
+    int duplicate = 0;
+    for (int i = 0; i < m; i++) {
+        if (monlst[i] == skillnum) {
+            duplicate++;
         }
     }
-    cout << "You got " << number_of_dulplicate << " dice correct." << endl;
-    return number_of_dulplicate;
-    }
+    cout << "You kill " << duplicate << " Monster." << endl;
+    return duplicate;
+}
 
-bool dice_rolling(string player1, string player2){//MiniGame Rolling Dice
-int choice = 0;
-    cout << "========================================================================================" << endl;
-    cout<< "Welcome to Mini-Game(Single-Player): Rolling dice"<<endl;
-    cout<<"You will play against a bot."<<endl;
-    cout<<"In this game, both players take turn make a correct guess within 4 randomly-generated dice"<<endl;
-    cout<<"A correct guess allows players to eliminate the number from its dice"<<endl;
-    cout<<"For example, Player 1 has 4 dice, he takes a guess of 3, fortunately, dice are [3,4,2,3] after the round."<<endl;
-    cout<<"Notice there are 2'3' in the dice"<<endl;
-    cout<<"He still has 2 more dice to guess"<<endl;
-    cout<<endl;
-    cout<<"Player with 0 dice left will win the game"<<endl;
-    cout<<"GAME BEGINS NOW!"<<endl;
-    cout<<"Lets go!"<<endl;
-    cout<<endl;
-   
-    vector<int> p1lst;
-    vector<int> p2lst;
-    int p1dice = 4;
-    int p2dice = 4;
-    rolling_dice(p1dice, p1lst);
-    rolling_dice(p2dice, p2lst);
+void playersturn(int skillnum, string player, int &m, vector<int> mlist){ //function that let player input their skill number and report if the monster can be killed.
+    int duplicate = 0;
+    while (true) {
+            cout << player << "'s turn!" << endl;
+            cout << "Number of monster left = " << m << endl;
+            cout << player << ", Your Number: ";
+            cin >> skillnum;
+            if ((skillnum < 1)||(skillnum > 9)){
+                cout << "Error: invalid input." << endl;
+                continue;}
+            else{
+                break;
+            }}
+        genmonster(m, mlist);
+        cout << "Monster are : " ;
+        for (int i=0; i < mlist.size(); i++){
+            cout << mlist[i] << "  ";
+        }
+        cout << endl;
+
+        duplicate = killmonster(skillnum, mlist, m);
+        m -= duplicate;
+
+        cout << player << " still have " << m << " to eliminate!" <<endl;
+        cout << endl;
+}
+
+int eliminate(string player1, string player2){// Mini game: Eliminate the numebr monster
+    vector<int> m1list;
+    vector<int> m2list;
+    int skillnum = 0;
+    int m1 = 3;
+    int m2 = 3;
     bool p1turn = true;
-    int a =0;
-    int b= 0;
+
+    cout << "========================================================================================" << endl;
+    cout << "𝓦𝓮𝓵𝓬𝓸𝓶𝓮 𝓽𝓸 𝓝𝓾𝓶𝓫𝓮𝓻 𝓜𝓸𝓷𝓼𝓽𝓮𝓻" << endl;
+    cout << "𝓣𝓱𝓮 𝓜𝓸𝓷𝓼𝓽𝓮𝓻 𝔀𝓲𝓵𝓵 𝓫𝓮 𝓮𝓵𝓲𝓶𝓲𝓷𝓪𝓽𝓮𝓭, 𝔀𝓱𝓮𝓷 𝓶𝓪𝓬𝓽𝓬𝓱𝓲𝓷𝓰 𝓽𝓱𝓮 𝓼𝓪𝓶𝓮 𝓷𝓾𝓶𝓫𝓮𝓻" << endl;
+    cout << "========================================================================================" << endl;
+    cout << "🄶 🄰 🄼 🄴  🄱 🄴 🄶 🄸 🄽 🅂  🄽 🄾 🅆 !" << endl;
+    cout << "🄻 🄴 🅃 🅂  🄶 🄾 !" << endl;
+    cout << endl;
+   
     while (true) {
         if (p1turn) {
-            while (true) {
-                cout<<"Now is "<<player1<<"'s turn!"<<endl;
-                cout << "Number of dice of " << player1 << "= " << p1dice << endl;
-                if (player1 == "Bot1" || player1 == "Bot2" || player1 == "Bot3") {
-                    choice = rand() % 6 + 1;
-                    std::cout << player1 << ", guess a number from your dice: " << choice << std::endl;}
-                else{
-                    cout << player1 << ", guess a number from your dice: ";
-                    cin >> choice;}
-                if ((choice<1)||(choice>6)){
-                    cout<<"Error: invalid input."<<endl;
-                    continue;}
-                else{
-                    break;
-                }}
-            rolling_dice(p1dice, p1lst);
-            cout << "Dice are : " ;
-            for (int i=0;i<p1lst.size();i++){
-                cout<<p1lst[i]<<"  ";
+            playersturn(skillnum, player1, m1, m1list);
+            p1turn = false;
             }
-            cout << endl;
-
-            a = check_number(choice, p1lst, p1dice);
-            p1dice -= a;
-
-            cout << player1 << " still have "<< p1dice <<" to guess!" <<endl;
-            cout<<endl;
-            p1turn = !p1turn;}
         else{
-            while (true) {
-                cout<<"Now is "<<player2<<"'s turn!"<<endl;
-
-                cout << "Number of dice of " << player2 << "= " << p1dice << endl;
-                if (player2 == "Bot1" || player2 == "Bot2" || player2 == "Bot3") {
-                    choice = rand() % 6 + 1;
-                    cout << player2 << ", guess a number from your dice: " << choice << endl;}
-                else{
-                    cout << player2 << ", guess a number from your dice: ";
-                    cin >> choice;}
-                if ((choice<1)||(choice>6)){
-                    cout<<"Error: invalid input."<<endl;
-                    continue;}
-                else{
-                    break;
-                }}
-        rolling_dice(p2dice, p2lst);
-        cout << "Dice are : " ;
-        for (int i=0;i<p2lst.size();i++){
-                cout<<p2lst[i]<<"  ";
-            }
-        cout << endl;
-        b = check_number(choice, p2lst, p2dice);
-
-        p2dice -= b;
-
-        cout << player2 << " still have "<< p2dice <<" to guess!"<< endl;
-        cout<<endl;
-        p1turn = !p1turn;}
-    if (p1dice <= 0) {
-        cout << "Winner is " << player1 << " ! Congratulations!\n";
-        cout << "You win 500m!"<<endl;
+            playersturn(skillnum, player2, m2, m2list);
+            p1turn = true;
+        }
+    if (m1 <= 0) {
+        cout << " ██████╗ ██████╗ ███╗   ██╗ ██████╗ ██████╗  █████╗ ████████╗██╗   ██╗██╗      █████╗ ████████╗██╗ ██████╗ ███╗   ██╗\n██╔════╝██╔═══██╗████╗  ██║██╔════╝ ██╔══██╗██╔══██╗╚══██╔══╝██║   ██║██║     ██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║\n██║     ██║   ██║██╔██╗ ██║██║  ███╗██████╔╝███████║   ██║   ██║   ██║██║     ███████║   ██║   ██║██║   ██║██╔██╗ ██║\n██║     ██║   ██║██║╚██╗██║██║   ██║██╔══██╗██╔══██║   ██║   ██║   ██║██║     ██╔══██║   ██║   ██║██║   ██║██║╚██╗██║\n╚██████╗╚██████╔╝██║ ╚████║╚██████╔╝██║  ██║██║  ██║   ██║   ╚██████╔╝███████╗██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║\n ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝" << endl;
+        cout << "Winner is " << player1 << endl;
         return true;
         break;
-    }
-    else if (p2dice <= 0) {
-        cout << "Winner is " << player2 <<endl;
-        cout<<"You lose 500m!"<<endl;
+        }
+    else if (m2 <= 0) {
+        cout << " ██████╗ ██████╗ ███╗   ██╗ ██████╗ ██████╗  █████╗ ████████╗██╗   ██╗██╗      █████╗ ████████╗██╗ ██████╗ ███╗   ██╗\n██╔════╝██╔═══██╗████╗  ██║██╔════╝ ██╔══██╗██╔══██╗╚══██╔══╝██║   ██║██║     ██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║\n██║     ██║   ██║██╔██╗ ██║██║  ███╗██████╔╝███████║   ██║   ██║   ██║██║     ███████║   ██║   ██║██║   ██║██╔██╗ ██║\n██║     ██║   ██║██║╚██╗██║██║   ██║██╔══██╗██╔══██║   ██║   ██║   ██║██║     ██╔══██║   ██║   ██║██║   ██║██║╚██╗██║\n╚██████╗╚██████╔╝██║ ╚████║╚██████╔╝██║  ██║██║  ██║   ██║   ╚██████╔╝███████╗██║  ██║   ██║   ██║╚██████╔╝██║ ╚████║\n ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝" << endl;
+        cout << "Winner is " << player2 << endl;
         return false;
         break;
+        }
     }
-}}
+}
+
+int main(){
+    string player1 = "Player 1", player2 = "Player 2";
+    eliminate( player1,  player2);
+}
